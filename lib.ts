f537456e968dbdea5a5b42b78cfddcf9eabfe79e
paldepind <simonfv@gmail.com>
2016-08-11 13:55:19 +0200

    Use Jabz

--- lib.ts
+++ lib.ts
@@ -2,44 +2,43 @@ import {Monad, Do} from "./monad";
 import {Maybe, nothing, just} from "../jabz/src/maybe";
 import {Either, left, right} from "../jabz/src/either";
 import {
-  Effects, withEffects, wrapEffects, runEffects, thunk
-} from "./effects";
-import * as Eff from "./effects";
+  IO, withEffects, wrapEffects, thunk, runIO, fromPromise
+} from "../jabz/src/io";
 
 type Time = number;
 
 export type E<A> = EImpl<A>;
 
 class EImpl<A> {
-  constructor(public val: Effects<Either<E<A>, A>>) {}
+  constructor(public val: IO<Either<E<A>, A>>) {}
   of<T>(t: T): E<T> {
-    return E(Eff.of(right(t)));
+    return E(IO.of(right(t)));
   }
   map<B>(f: (a: A) => B): E<B> {
     return E<B>(this.val.chain(e => e.match({
-      right: v => Eff.of(right(f(v))),
-      left: v => Eff.of(left(E(thunk(() => v.map(f).val))))
+      right: v => IO.of(right(f(v))),
+      left: v => IO.of(left(E(thunk(() => v.map(f).val))))
     })));
   }
   chain<T>(f: (t: A) => E<T>): E<T> {
     return E<T>(this.val.chain(v => v.match({
-      left: (e: E<A>) => Eff.of(left(e.chain(f))),
+      left: (e: E<A>) => IO.of(left(e.chain(f))),
       right: (t: A) => (f(t)).val
     })));
   }
 }
 
 export function ofE<A>(a: A): E<A> {
-  return E(Eff.of(right(a)));
+  return E(IO.of(right(a)));
 }
 
-function E<A>(e: Effects<Either<E<A>, A>>) {
+function E<A>(e: IO<Either<E<A>, A>>) {
   return new EImpl<A>(e);
 }
 
 function minTime<A, B>(e1: E<A>, e2: E<B>): E<{}> {
   return E(runE(e1).chain(v1 => runE(e2).chain(v2 => {
-    return Eff.of(v1.match({
+    return IO.of(v1.match({
       right: _ => right({}),
       left: l1 => v2.match({
         right: _ => right({}),
@@ -49,24 +48,24 @@ function minTime<A, B>(e1: E<A>, e2: E<B>): E<{}> {
   })));
 }
 
-export function runE<A>(e: E<A>): Effects<Either<E<A>, A>> {
+export function runE<A>(e: E<A>): IO<Either<E<A>, A>> {
   return e.val;
 }
 
-export const never = E(Eff.of(left(undefined)));
-never.val = Eff.of(left(never)); // cyclic
+export const never = E(IO.of(left(undefined)));
+never.val = IO.of(left(never)); // cyclic
 
 type InB<A> = {cur: A, next: E<Behavior<A>>}
 
 export class Behavior<A> {
-  constructor(public val: Effects<InB<A>>) {
+  constructor(public val: IO<InB<A>>) {
     this.val = val;
   }
   of<B>(b: B): Behavior<B> {
-    return B(Eff.of({cur: b, next: never}));
+    return B(IO.of({cur: b, next: never}));
   }
   static of<B>(b: B): Behavior<B> {
-    return B(Eff.of({cur: b, next: never}));
+    return B(IO.of({cur: b, next: never}));
   }
   map<B>(f: (a: A) => B): Behavior<B> {
     return B(this.val.map(({cur, next}) => {
@@ -89,32 +88,29 @@ export class Behavior<A> {
   }
 }
 
-export function runB<A>(b: Behavior<A>): Effects<InB<A>> {
+export function runB<A>(b: Behavior<A>): IO<InB<A>> {
   return b.val;
 }
 
 export function apB<A, B>(bf: Behavior<(a: A) => B>, ba: Behavior<A>): Behavior<B> {
   return B<B>(
-    runB(bf).chain(f => runB(ba).chain(a => Eff.of({
+    runB(bf).chain(f => runB(ba).chain(a => IO.of({
       cur: f.cur(a.cur),
       next: minTime(f.next, a.next)
     })))
   );
 }
 
-export function B<A>(v: Effects<InB<A>>): Behavior<A> {
+export function B<A>(v: IO<InB<A>>): Behavior<A> {
   return new Behavior(v);
 }
 
 export function switcher<A>(b: Behavior<A>, e: E<Behavior<A>>): Behavior<A> {
-  // console.log("entering switcher ************");
-  // console.log(e);
   return B(runE(e).chain(either => {
-    // console.log("in switcher **************");
-    return either.match<Effects<InB<A>>>({
+    return either.match<IO<InB<A>>>({
       left: (ne) => {
         return runB(b).chain(({cur, next}) => {
-          return Eff.of({cur, next: switchE(next, ne)});
+          return IO.of({cur, next: switchE(next, ne)});
         });
       },
       right: (b) => runB(b)
@@ -126,19 +122,14 @@ function switchE<A>(e1: E<Behavior<A>>, e2: E<Behavior<A>>): E<Behavior<A>> {
   return minTime(e1, e2).map(_ => switcher(switcher(Behavior.of(undefined), e1), e2));
 }
 
-// function switcher<A>(b: Behavior<A>, e: E<Behavior<A>>): Behavior<A> {
-//   const [t, s] = e;
-//   return new Behavior((n) => n < t ? b.fn(n) : s.fn(n));
-// }
-
 export function whenJust<A>(b: Behavior<Maybe<A>>): Behavior<E<A>> {
   return B(runB(b).chain((v: InB<Maybe<A>>) => {
     console.log("In when just chain, current is:", v.cur);
     return v.cur.match({
-      just: (x) => Eff.of({cur: ofE(x), next: v.next.map(whenJust)}),
+      just: (x) => IO.of({cur: ofE(x), next: v.next.map(whenJust)}),
       nothing: () => planM(v.next.map(b => runB(whenJust(b)))).chain((en) => {
         console.log("Deeper in whenJust, matched nothing, made new plan");
-        return Eff.of({cur: en.chain(o => o.cur), next: en.chain(o => o.next)});
+        return IO.of({cur: en.chain(o => o.cur), next: en.chain(o => o.next)});
       })
     });
   }));
@@ -158,7 +149,7 @@ class Clock {
 }
 
 type Plan<A> = {
-  computation: E<Effects<A>>,
+  computation: E<IO<A>>,
   result: Maybe<A> // mutable
   , name: string // temp
 }
@@ -169,15 +160,15 @@ type Ref<A> = {
 
 type PrimE<A> = {ref: Maybe<[Round, A]>}; // mutable ref
 
-function spawn<A>(c: Clock, e: Effects<A>): Effects<PrimE<A>> {
+function spawn<A>(c: Clock, e: IO<A>): IO<PrimE<A>> {
   let mv = {ref: nothing()};
-  Eff.runEffects(e).then(res => {
+  runIO(e).then(res => {
     console.log("");
     console.log("Spawned stuff finished with result", res);
     mv.ref = just([c.round, res]);
     mainLoop();
   });
-  return Eff.of(mv);
+  return IO.of(mv);
 }
 
 function observeAt<A>(re: PrimE<A>, r: Round): Maybe<A> {
@@ -196,15 +187,15 @@ let resolve: Function;
 let endE: E<any>;
 
 export class Now<A> {
-  comp: Effects<A>;
-  constructor(a: Effects<A>) {
+  comp: IO<A>;
+  constructor(a: IO<A>) {
     this.comp = a;
   }
   of<B>(b: B) {
-    return new Now(Eff.of(b));
+    return new Now(IO.of(b));
   }
   static of<B>(b: B) {
-    return new Now(Eff.of(b));
+    return new Now(IO.of(b));
   }
   chain<B>(f: (a: A) => Now<B>): Now<B> {
     return new Now(this.comp.chain((v) => f(v).comp));
@@ -214,26 +205,26 @@ export class Now<A> {
   }
 }
 
-function getNow<A>(n: Now<A>): Effects<A> {
+function getNow<A>(n: Now<A>): IO<A> {
   return n.comp;
 }
 
 // lift an Effect into the Now monad
-function liftIO<A>(e: Effects<A>): Now<A> {
+function liftIO<A>(e: IO<A>): Now<A> {
   return new Now(e);
 }
 
-export function effMapM_<A, B>(f: (a: A) => Effects<B>, arr: A[]): Effects<{}> {
-  return arr.reduce((acc, cur) => acc.chain(_ => f(cur)), Eff.of({}));
+export function effMapM_<A, B>(f: (a: A) => IO<B>, arr: A[]): IO<{}> {
+  return arr.reduce((acc, cur) => acc.chain(_ => f(cur)), IO.of({}));
 }
 
 // Now API
 
 const readClock = wrapEffects(() => clock);
 
-const curRound: Effects<Round> = withEffects(() => clock.round)();
+const curRound: IO<Round> = withEffects(() => clock.round)();
 
-export function async<A>(e: Effects<A>): Now<E<A>> {
+export function async<A>(e: IO<A>): Now<E<A>> {
   return liftIO(readClock.chain((c: Clock) => spawn(c, e))).map(toE);
 }
 
@@ -249,14 +240,14 @@ function toE<A>(pe: PrimE<A>): E<A> {
 function planToEv<A>(p: Plan<A>): E<A> {
   console.log("---1 planToEv called");
   const ef = () => p.result.match({
-    just: (x) => Eff.of(right(x)), // plan has been runned, return result
+    just: (x) => IO.of(right(x)), // plan has been runned, return result
     nothing: () => {
       console.log("---2 Running plan");
       return runE(p.computation).chain(estate => {
         console.log("---3 Returning plan state as ev:", estate);
         return estate.match({
           left: (_) => {
-            return Eff.of(left(E(thunk(() => {
+            return IO.of(left(E(thunk(() => {
               console.log("Calling planToEv from thunk", plan.name);
               return runE(planToEv(p));
             }))));
@@ -264,7 +255,7 @@ function planToEv<A>(p: Plan<A>): E<A> {
           right: (m) => m.chain(v => {
             console.log("---4 Result:", v);
             p.result = just(v); // mutate result into plan
-            return Eff.of(right(v));
+            return IO.of(right(v));
           })
         });
       });
@@ -289,7 +280,7 @@ export function plan<A>(pl: E<Now<A>>): Now<E<A>> {
   })());
 }
 
-function planM<A>(pl: E<Effects<A>>): Effects<E<A>> {
+function planM<A>(pl: E<IO<A>>): IO<E<A>> {
   return withEffects(() => {
     const p: Plan<A> = {
       computation: pl,
@@ -305,12 +296,12 @@ function addPlan(plan: Ref<Plan<any>>): void {
   plans.push(plan);
 }
 
-function tryPlans(): Effects<{}> {
+function tryPlans(): IO<{}> {
   console.log("--------------- Trying plans, nr. of plans:", plans.length);
   return effMapM_(tryPlan, plans);
 }
 
-function tryPlan<A>(refPlan: Ref<Plan<A>>): Effects<{}> {
+function tryPlan<A>(refPlan: Ref<Plan<A>>): IO<{}> {
   const plan = refPlan.ref;
   console.log("------------*** Trying plan from ", plan.name);
   return runE(planToEv(plan)).chain((eres) => {
@@ -321,22 +312,22 @@ function tryPlan<A>(refPlan: Ref<Plan<A>>): Effects<{}> {
         let nrOf = plans.length;
         pull(refPlan, plans); // plan is done, remove
         console.log("nr of plans whent from", nrOf, "to", plans.length);
-        return Eff.of({});
+        return IO.of({});
       },
-      left: () => Eff.of({})
+      left: () => IO.of({})
     });
   });
 }
 
-export function runNow<A>(n: Now<E<A>>): Effects<A> {
+export function runNow<A>(n: Now<E<A>>): IO<A> {
   console.log("runNow");
   plans = [];
   clock = new Clock();
   endE = undefined;
-  return Eff.fromPromise(new Promise((res, rej) => {
+  return fromPromise(new Promise((res, rej) => {
     resolve = res;
     console.log("1/2: Starting initial now comp", n.comp);
-    Eff.runEffects(n.comp).then((ev) => {
+    runIO(n.comp).then((ev) => {
       console.log("2/2: Finished running initial now comp");
       console.log(ev);
       if (!(ev instanceof EImpl)) {
@@ -355,16 +346,16 @@ export function runNow<A>(n: Now<E<A>>): Effects<A> {
 // the initial Now computation given to `runNow`
 function mainLoop<A>(): void {
   console.log("1/ mainLoop", endE, endE ? "" : "<-- is undef :(");
-  Eff.runEffects(tryPlans().chain(_ => {
+  runIO(tryPlans().chain(_ => {
     console.log("-------------- Done trying plans");
     return runE(endE).chain((v: Either<E<A>, A>) => {
       console.log("2/ pulled in endE");
       console.log(v);
       return v.match({
-        left: () => Eff.of({}),
+        left: () => IO.of({}),
         right: (a: A) => {
           resolve(a);
-          return Eff.of({});
+          return IO.of({});
         }
       });
     });
--- test/main.ts
+++ test/main.ts
@@ -1,19 +1,16 @@
-///<reference path="../typings/index.d.ts" />
-import "babel-polyfill";
 import * as assert from "assert";
 
 import {
   Behavior, E, runB, apB, B, never, runE, ofE, async, runNow,
   Now, plan, switcher, whenJust, when, sample
 } from "../lib";
-import {Maybe, just, nothing} from "../maybe";
+import {just, nothing} from "../../jabz/src/maybe";
 import {Do} from "../monad";
-import * as Eff from "../effects";
-import {Effects, withEffects, withEffectsP, runEffects} from "../effects";
+import {IO, withEffects, withEffectsP, runIO} from "../../jabz/src/io";
 
 describe("Event", () => {
   it("never is cyclic", () => {
-    return runEffects(runE(never)).then((e) => {
+    return runIO(runE(never)).then((e) => {
       e.match({
         left: (e) => assert.equal(never, e),
         right: () => { throw new Error(); }
@@ -25,7 +22,7 @@ describe("Event", () => {
       const e = yield Now.of(ofE(12));
       return Now.of(e.map((x: number) => x * 2));
     });
-    return runEffects(runNow(prog)).then(res => {
+    return runIO(runNow(prog)).then(res => {
       assert.equal(24, res);
     });
   });
@@ -36,14 +33,14 @@ describe("Behavior", () => {
     const b1 = Behavior.of((x: number) => x * x);
     const b2 = Behavior.of(12);
     const append = apB(b1, b2);
-    return runEffects(runB(append)).then((res) => {
+    return runIO(runB(append)).then((res) => {
       assert.equal(res.cur, 12 * 12);
     });
   });
   it("map works on constant behavior", () => {
     const b = Behavior.of(12);
     const b2 = b.map((n) => n * 2);
-    return runEffects(runB(b2)).then((res) => {
+    return runIO(runB(b2)).then((res) => {
       assert.equal(res.cur, 24);
     });
   });
@@ -53,7 +50,7 @@ describe("Behavior", () => {
       const n = yield sample(b);
       return Now.of(ofE(n));
     });
-    return runEffects(runNow(prog)).then((res) => {
+    return runIO(runNow(prog)).then((res) => {
       assert.strictEqual(res, 18);
     });
   });
@@ -63,7 +60,7 @@ describe("Behavior", () => {
       const ev = yield sample(whenJust(b));
       return Now.of(ev);
     });
-    return runEffects(runNow(prog)).then((res) => {
+    return runIO(runNow(prog)).then((res) => {
       assert.strictEqual(res, 77);
     });
   });
@@ -73,13 +70,13 @@ describe("Behavior", () => {
       const _ = yield sample(whenJust(b));
       return Now.of(ofE(8));
     });
-    return runEffects(runNow(prog)).then((res) => {
+    return runIO(runNow(prog)).then((res) => {
       assert.strictEqual(res, 8);
     });
   });
   it("behavior switches to event of behavior", () => {
     let resolve: (b: Behavior<boolean>) => void;
-    function getTrueBeh(): Effects<Behavior<number>> {
+    function getTrueBeh(): IO<Behavior<number>> {
       return withEffectsP(() => {
         return new Promise((res) => {
           resolve = res;
@@ -95,13 +92,13 @@ describe("Behavior", () => {
     setTimeout(() => {
       resolve(Behavior.of(true));
     });
-    return runEffects(runNow(prog)).then((res) => {
+    return runIO(runNow(prog)).then((res) => {
       assert.strictEqual(res, 4);
     });
   });
   it("samples when correctly", () => {
     let resolves: ((n: number) => void)[] = [];
-    function getNextNr(idx: number): Effects<number> {
+    function getNextNr(idx: number): IO<number> {
       return withEffectsP(() => {
         return new Promise((res) => {
           resolves[idx] = res;
@@ -132,14 +129,14 @@ describe("Behavior", () => {
         });
       });
     });
-    return runEffects(runNow(main())).then((res) => {
+    return runIO(runNow(main())).then((res) => {
       assert.deepEqual({}, res);
       console.log("\\\\\\\\\\\\\\\\\\\\ Final res:", res);
     });
   });
   it("handles recursively defined behavior", () => {
     let resolve: (n: number) => void;
-    function getNextNr(): Effects<number> {
+    function getNextNr(): IO<number> {
       return withEffectsP(() => {
         return new Promise((res) => {
           resolve = res;
@@ -173,8 +170,8 @@ describe("Behavior", () => {
         });
       });
     });
-    return runEffects(runNow(main())).then((res) => {
-      console.log("\\\\\\\\\\\\\\\\\\\\ Final res:", res);
+    return runIO(runNow(main())).then((res) => {
+      assert.deepEqual(res, {});
     });
   });
 });
@@ -185,7 +182,7 @@ describe("Now", () => {
       const a = yield Now.of(12);
       return Now.of(ofE(a));
     });
-    return runEffects(runNow(prog)).then(res => {
+    return runIO(runNow(prog)).then(res => {
       assert.equal(12, res); // actually 20
     });
   });
@@ -194,7 +191,7 @@ describe("Now", () => {
       const a = yield async(withEffects((n: number) => n)(12));
       return Now.of(a);
     });
-    return runEffects(runNow(prog)).then(res => {
+    return runIO(runNow(prog)).then(res => {
       assert.equal(12, res); // actually 20
     });
   });
@@ -209,7 +206,7 @@ describe("Now", () => {
       });
       return Now.of(c);
     });
-    return runEffects(runNow(prog)).then(res => {
+    return runIO(runNow(prog)).then(res => {
       assert.equal(20, res); // actually 20
     });
   });
@@ -224,7 +221,7 @@ describe("Now", () => {
       });
       return Now.of(c);
     });
-    return runEffects(runNow(prog)).then(res => {
+    return runIO(runNow(prog)).then(res => {
       assert.equal(20, res); // actually 20
     });
   });
@@ -239,7 +236,7 @@ describe("Now", () => {
     const prog = Do(function*(): Iterator<Now<any>> {
       return async(fn());
     });
-    runEffects(runNow(prog)).then((res: boolean) => {
+    runIO(runNow(prog)).then((res: boolean) => {
       done = res;
     });
     setTimeout(() => {
@@ -253,7 +250,7 @@ describe("Now", () => {
   });
   it("async event can be mapped", () => {
     let resolve: (n: number) => void;
-    function getNextNr(): Effects<number> {
+    function getNextNr(): IO<number> {
       return withEffectsP(() => {
         return new Promise((res) => {
           resolve = res;
@@ -276,7 +273,7 @@ describe("Now", () => {
       resolve(6);
     });
     console.log("running main");
-    return runEffects(runNow(loop(0))).then((result) => {
+    return runIO(runNow(loop(0))).then((result) => {
       console.log("Finished with result", result);
     });
   });
@@ -301,7 +298,7 @@ describe("Now", () => {
       assert.strictEqual(done, false);
       resolve(11);
     });
-    return runEffects(runNow(prog)).then((res: number) => {
+    return runIO(runNow(prog)).then((res: number) => {
       done = true;
       assert.strictEqual(res, 22);
     });
@@ -319,35 +316,4 @@ describe("Now", () => {
   //     const e1 = sample(when(b.map((n: number) => n === 10)));
   //   })
   // });
-});
-
-describe("maybe", () => {
-  it("gives just on `of`", () => {
-    const j = just(12);
-    assert.deepEqual(j, j.of(12));
-  });
-  it("gives nothing when bound to nothing", () => {
-    const j = just(12);
-    const n = nothing();
-    assert.deepEqual(j.chain(_ => nothing()), n);
-    assert.deepEqual(n.chain<number>(_ => just(12)), n);
-  });
-  it("passes values through", () => {
-    const res: Maybe<number> = Do(function*() {
-      const a = yield just(1);
-      const b = yield just(3);
-      const c = yield just(2);
-      return just(a + b + c);
-    });
-    assert.deepEqual(res, just(6));
-  });
-  it("bails on nothing", () => {
-    const res = Do(function*() {
-      const a = yield just(1);
-      const b = yield nothing();
-      const c = yield just(2);
-      return just(a + b + c);
-    });
-    assert.deepEqual(res, nothing());
-  });
 });
